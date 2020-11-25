fd0a10ec57ef8a7c48bad17b5b9d3f2d4e5efb57
nicrausaz <n.crausaz99@gmail.com>
2020-11-25 11:10:40 +0100

    Inserts merge

---
+++ 4. SQL - Insert/Script Insert/bdr_proj_crausaz_scharwath_public_administrator_club.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.administrator_club (administratoruid, clubid) VALUES ('33cee9ed-9727-4e14-b93b-6fb9f88209e5', 1);
+INSERT INTO public.administrator_club (administratoruid, clubid) VALUES ('c769bceb-729c-4264-a824-c3fae90fcd51', 2);
+INSERT INTO public.administrator_club (administratoruid, clubid) VALUES ('312173ac-63e7-40b5-ae67-be254046b8ed', 3);
