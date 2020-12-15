b50c0bbedb1c747092962111e87ceb9a5b6cc8c2
Nicolas Crausaz <n.crausaz99@gmail.com>
2020-12-15 12:28:09 +0100

    Template 2

--- 5. APP/frontend/src/App.vue
+++ 5. APP/frontend/src/App.vue
@@ -2,45 +2,56 @@
   <v-app id="inspire">
     <v-navigation-drawer
       v-model="drawer"
-      absolute
-      temporary
+      app
     >
-      <v-list-item>
-        <v-list-item-avatar>
-          <v-img src="https://randomuser.me/api/portraits/men/78.jpg"></v-img>
-        </v-list-item-avatar>
-
-        <v-list-item-content>
-          <v-list-item-title>Test</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-
-      <v-divider></v-divider>
-
-      <v-list dense>
-        <v-list-item
-          v-for="item in links"
-          :key="item.text"
-        >
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title>{{ item.text }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
+      <!--  -->
     </v-navigation-drawer>
 
-    <v-app-bar app>
+    <v-app-bar app dense>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
       <v-toolbar-title>Application</v-toolbar-title>
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-menu
+        left
+        bottom
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            icon
+            v-bind="attrs"
+            v-on="on"
+          >
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item
+            v-for="n in 5"
+            :key="n"
+            @click="() => {}"
+          >
+            <v-list-item-title>Option {{ n }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
     </v-app-bar>
 
     <v-main>
-      <router-view/>
+      <v-container>
+        <router-view/>
+      </v-container>
+
     </v-main>
   </v-app>
 </template>
@@ -56,9 +67,9 @@ import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
   })
 export default class App extends Vue {
     drawer = false;
-    links: Array<object> = [
+    items: Array<object> = [
       {
-        text: 'test'
+        title: 'test'
       }
     ]
 }
